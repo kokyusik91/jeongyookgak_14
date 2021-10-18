5546b8323a63d3a89436a43937cd680f6371e9fe
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2021-10-18 17:06:23 +0900

    feature. axios 추가 및 backend 통신확인 완료

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,9 +1,25 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios({
+      url: 'http://3.36.92.203/',
+      method: 'GET',
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className='App'>
       <h1>안녕하세요!!</h1>
+      <button>연결 test</button>
     </div>
   );
 }
